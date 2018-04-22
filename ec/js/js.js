b8f8f20e43a82ff6eda5fda51ab24a75b045b8c5
Martijn Steenbergen <m.uses+git@outlook.com>
2018-04-22 17:03:58 +0200

    Create a new website

--- ec/js/js.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".center").each(function (n, e) {
-        var heightofChild = e.lastElementChild.offsetTop + e.lastElementChild.offsetHeight;
-        var heightofMe = e.offsetTop + e.scrollHeight;
-        e.style.padding = (heightofMe - heightofChild) / 2 + "px";
-    });
-});
